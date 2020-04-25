cfeff36c0ad168a694568798f4aea07f4927a9b0
soohyeon <berry5750@naver.com>
2020-04-25 15:06:11 +0900

    map

---
+++ src/components/Map.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="map">
+    <naver-maps
+      :height="height"
+      :width="width"
+      :mapOptions="mapOptions"
+      :initLayers="initLayers"
+      @load="onLoad">
+    </naver-maps>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data(){
+    return{
+      width: 300,
+      height: 300,
+      map: null,
+      isCTT: false,
+      mapOptions: {
+        lat: 37,
+        lng: 127,
+        zoom: 10,
+        zoomControlOptions: {position: 'TOP_RIGHT'},
+      },
+        initLayers: ['BACKGROUND', 'BACKGROUND_DETAIL', 'POI_KOREAN']
+      }
+    },
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// naver-map api
+import naver from 'vue-naver-maps';
+Vue.use(naver, {
+  clientID: 'gv7ysohqky',
+  useGovAPI: false, //공공 클라우드 API 사용 (선택)
+  subModules:'' // 서브모듈 (선택)
+});
 
 new Vue({
   render: h => h(App),

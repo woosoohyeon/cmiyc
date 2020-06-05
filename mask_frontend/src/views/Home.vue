3f26a3830512209108eb4d7807f682bc6167d066
soohyeon <berry5750@naver.com>
2020-06-06 00:30:02 +0900

    메인페이지 로고, 검색창 수정

--- mask_frontend/src/views/Home.vue
+++ mask_frontend/src/views/Home.vue
@@ -3,7 +3,7 @@
   <b-container>
     <!-- 제목 시작 -->
     <b-row align-h="center" class="title">
-      <h1>Catch Mask If You Can</h1>
+    <img src="../../public/mainLogo.png" alt="My Image">
     </b-row>
     <b-row align-h="center">
       <Purchasable/>
@@ -14,7 +14,7 @@
     </b-row>
     <b-row align-h="center" class="mt-3 mb-4">
       <!-- 현재위치 받은 후 지도로 이동 -->
-      <b-button variant="info" @click="nowLocate">현재위치로 확인하기</b-button>
+      <b-button variant="info" @click="nowLocate" id = "CurrentPosion">현재위치로 확인하기</b-button>
     </b-row>
       <!-- 주소 검색 -->
       <Search/>
@@ -57,5 +57,12 @@ export default {
 a{
   color: black;
   text-decoration: none;
+}
+img{
+	width : 40%;
+}
+#CurrentPosion{
+  border: 1px solid #374baa;
+  background-color: #374baa;
 }
 </style>
